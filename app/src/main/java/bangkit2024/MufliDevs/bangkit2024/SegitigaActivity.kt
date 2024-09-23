808f88a6e12386702ea81bb720554663619da2e4
acoramadan <laxmiwatimaani@gmail.com>
2024-09-23 13:21:50 +0800

    some changes

---
+++ app/src/main/java/bangkit2024/MufliDevs/bangkit2024/SegitigaActivity.kt
@@ -0,0 +1,21 @@
+package bangkit2024.MufliDevs.bangkit2024
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.MufliDevs.bangkit2024.R
+
+class SegitigaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_segitiga)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
